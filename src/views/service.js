fa9b868fe2bf3b882c2cd40759bb58d67d64c54c
pankowskitomasz <pankowski_tomasz@o2.pl>
2021-07-30 23:04:30 +0200

    frontend: login, register, contact

---
+++ src/views/service.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+
+class Service extends Component{
+    render(){
+        return(          
+            <main className="minh-100vh">     
+
+            </main>
+        );
+    }
+}
+
+export default Service;
